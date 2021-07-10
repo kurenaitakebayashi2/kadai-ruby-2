d5308bbad07c6fb90532e87bb016d41e1dc9fecf
kurenaitakebayashi2 <h03gm508@yahoo.co.jp>
2021-07-10 11:42:05 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -2,13 +2,13 @@ num_max = 100
 (1..num_max).each do |num|
   def fizzBuzz(num)
     if num % 3 == 0 && num % 5 == 0
-      puts "FizzBuzz"      
+      return "FizzBuzz"      
     elsif num % 3 == 0
-      puts "Fizz"
+      return "Fizz"
     elsif num % 5 == 0
-      puts "Buzz"
+      return "Buzz"
     else
-      puts num
+      return num
     end
   end  
   puts fizzBuzz(num) 
